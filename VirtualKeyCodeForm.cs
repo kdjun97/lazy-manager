f6b773c03349692edea6ae5b5acc157045e0262b
kdjun97 <kdjun97@gmail.com>
2022-04-01 22:13:00 +0900

    Feat: Add virtual key code form
    가상 키코드를 보여줄 폼 구현

---
+++ VirtualKeyCodeForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lazy_manager
+{
+    public partial class VirtualKeyCodeForm : Form
+    {
+        public VirtualKeyCodeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
