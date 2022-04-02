a40054189d2acf30f42516f80763bab7578bb8a5
kdjun97 <kdjun97@gmail.com>
2022-04-03 01:12:19 +0900

    Fix: Model constructor
    HotkeyModel의 list<Tuple> contstructor 문제 해결

--- Model/HotkeyModel.cs
+++ Model/HotkeyModel.cs
@@ -9,7 +9,7 @@ namespace lazy_manager.Model
     class HotkeyModel
     {
         string hotkey; // 핫키 정보
-        List<Tuple<char, string>> command; // 명령어 리스트
+        List<Tuple<char, string>> command = new List<Tuple<char, string>>(); // 명령어 리스트
         bool isExit; // exitapp or repeat
 
         public string GetHotkey() => hotkey;
