71cac1b9ba4a20f4c0226ff3a1a59aa131e9193f
kdjun97 <kdjun97@gmail.com>
2022-04-03 22:00:16 +0900

    Feat: Move and Rename HotkeySetting func
    HotkeySetting func -> SetHotkeyModelList
    move this function to Script/

--- Script/ReadScript.cs
+++ Script/ReadScript.cs
@@ -54,51 +54,5 @@ namespace lazy_manager.Script
             }
             return list;
         }
-
-        // 핫키를 셋팅해주는 함수
-        public List<HotkeyModel> HotkeySetting(List<Tuple<char, string>> parsedData)
-        {
-            // 초기 버전은 loop 없이 q만 허용
-            List<HotkeyModel> hotkeyList = new List<HotkeyModel>(); // return해줄 hotkeyList
-            
-            try
-            {
-                for (int index = 0; index < parsedData.Count(); index++)
-                {
-                    if (parsedData[index].Item1 == 'h')
-                    {
-                        Debug.Print(index+"번에 h찾음");
-                        HotkeyModel hotkeyModel = new HotkeyModel(); // 하나의 핫키 모델
-                        hotkeyModel.SetHotkey(parsedData[index].Item2); // 핫키 설정
-
-                        int j = index + 1;
-                        while (true)
-                        {
-                            if (j > parsedData.Count()) // exception, q가 없을경우 무한루프
-                                throw new Exception("Loop Exception");
-                            if (parsedData[j].Item1 == 'q')
-                            {
-                                hotkeyModel.SetIsExit(true);
-                                hotkeyList.Add(hotkeyModel); // 완성된 모델 추가
-
-                                index = j;
-                                break;
-                            }
-                            else
-                            {
-                                hotkeyModel.SetCommand(new Tuple<char, string>(parsedData[j].Item1, parsedData[j].Item2));
-                                j++;
-                            }
-                        }
-                    }
-                    
-                }
-            } catch (Exception eMsg)
-            {
-                Debug.Print(eMsg.Message);
-            }
-
-            return hotkeyList;
-        }
     }
 }
