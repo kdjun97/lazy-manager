b4fdb7d11ca0070fb40bd9297c1b171ff2836759
kdjun97 <kdjun97@gmail.com>
2022-04-04 21:05:59 +0900

    Feat: Add hotkey customizing
    핫키 커스터마이징 기능 추가
    기존 0x70 이런 key code방식이 아닌, F1 이런식으로 직관적인 인풋을 받아야함
    script 형식 변경 필요

--- Script/HotkeySetting.cs
+++ Script/HotkeySetting.cs
@@ -11,54 +11,6 @@ namespace lazy_manager.Script
 {
     class HotkeySetting
     {
-        
-        // 핫키 모델 리스트를 셋팅해주는 함수
-        public List<HotkeyModel> SetHotkeyModelList(List<Tuple<char, string>> parsedData)
-        {
-            // 초기 버전은 loop 없이 q만 허용
-            List<HotkeyModel> hotkeyList = new List<HotkeyModel>(); // return해줄 hotkeyList
-
-            try
-            {
-                for (int index = 0; index < parsedData.Count(); index++)
-                {
-                    if (parsedData[index].Item1 == 'h')
-                    {
-                        Debug.Print(index + "번에 h찾음");
-                        HotkeyModel hotkeyModel = new HotkeyModel(); // 하나의 핫키 모델
-                        hotkeyModel.SetHotkey(parsedData[index].Item2); // 핫키 설정
-
-                        int j = index + 1;
-                        while (true)
-                        {
-                            if (j > parsedData.Count()) // exception, q가 없을경우 무한루프
-                                throw new Exception("Loop Exception");
-                            if (parsedData[j].Item1 == 'q')
-                            {
-                                hotkeyModel.SetIsExit(true);
-                                hotkeyList.Add(hotkeyModel); // 완성된 모델 추가
-
-                                index = j;
-                                break;
-                            }
-                            else
-                            {
-                                hotkeyModel.SetCommand(new Tuple<char, string>(parsedData[j].Item1, parsedData[j].Item2));
-                                j++;
-                            }
-                        }
-                    }
-
-                }
-            }
-            catch (Exception eMsg)
-            {
-                Debug.Print(eMsg.Message);
-            }
-
-            return hotkeyList;
-        }
-
         // 핫키를 셋팅해주는 함수
         public List<Keys> SetHotkey(List<HotkeyModel> hotkeyModel)
         {
@@ -66,52 +18,16 @@ namespace lazy_manager.Script
 
             try
             {
-                Keys k = (Keys)Enum.Parse(typeof(Keys), "LControlKey", true);
-                hookedKeys.Add(k);
                 for (int index = 0; index < hotkeyModel.Count(); index++)
                 {
-                    string tempKeyCode = "0x" + hotkeyModel[index].GetHotkey();
-                    hookedKeys.Add(StringToKey(tempKeyCode));
-
+                    Keys keys = (Keys)Enum.Parse(typeof(Keys), hotkeyModel[index].GetHotkey());
+                    hookedKeys.Add(keys);
                 }
             } catch (Exception eMsg)
             {
                 Debug.Print(eMsg.Message);
             }
             return hookedKeys;
-        }
-
-        // input 키코드를 처리해줌
-        public Keys StringToKey(string keyCode)
-        {
-            Keys key = new Keys();
-
-            try
-            {
-                switch (keyCode)
-                {
-                    case "0x70":
-                        Debug.Print("0x70찾음");
-                        key = Keys.F1;
-                        break;
-                    case "0x71":
-                        key = Keys.F2;
-                        break;
-                    case "0x72":
-                        key = Keys.F3;
-                        break;
-                    default:
-                        key = Keys.None;
-                        // throw new Exception("Key Code Error");
-                        break;
-                }
-            } catch (Exception eMsg)
-            {
-                Debug.Print(eMsg.Message);
-            }
-            
-
-            return key;
         }
     }
 }
