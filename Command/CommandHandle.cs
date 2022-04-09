5000f48e24216aa2e24bf067dbada4213bb92211
kdjun97 <kdjun97@gmail.com>
2022-04-09 13:58:53 +0900

    Feat: Add CommandHandle class
    Add HotkeyCommandHandle function
    Read HotkeyModel and getCommand from hotkeyModel
    And then, handle each commands' event such as k, m, s and q

---
+++ Command/CommandHandle.cs
@@ -0,0 +1,46 @@
+﻿using lazy_manager.Model;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using lazy_manager.Event;
+
+namespace lazy_manager.Command
+{
+    class CommandHandle
+    {
+        KeyboardEvent keyboardEvent = new KeyboardEvent();
+
+        /// <summary>
+        /// 핫키의 명령어들을 핸들해줌.
+        /// </summary>
+        /// <param name="hotkeyModel"></param>
+        public async void HotkeyCommandHandle(HotkeyModel hotkeyModel)
+        {
+            try
+            {
+                foreach (Tuple<char, string> command in hotkeyModel.GetCommand())
+                {
+                    switch (command.Item1) {
+                        case 'k': // keybd_event
+                            keyboardEvent.KeyboardEventHandle(command.Item2);
+                            break;
+                        case 'm': // mouse_event
+                            break;
+                        case 's': // sleep
+                            await Task.Delay(int.Parse(command.Item2));
+                            break;
+                        case 'q': // 명령어의 끝
+                            break;
+                        default:
+                            throw new Exception("예외: 등록되지 않은 명령어");
+                    }
+                }
+
+            } catch (Exception eMsg)
+            {
+                Debug.Print(eMsg.Message);
+            }
+
+        }
+    }
+}
