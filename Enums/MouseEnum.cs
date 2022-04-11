ef0b2da4b5fc56a7bd2ceb5d8f08d52f39171e87
kdjun97 <kdjun97@gmail.com>
2022-04-11 21:44:47 +0900

    Style: Add comment
    mouse_event에 관한 열거형

--- Enums/MouseEnum.cs
+++ Enums/MouseEnum.cs
@@ -8,11 +8,11 @@
         ABSOLUTE = 0x8000, // 전역위치
         MOUSEMOVE = 0x0001, // 마우스 이동
         LEFTDOWN = 0x0002, // 마우스 좌클릭
-        LEFTUP = 0x0004,
+        LEFTUP = 0x0004, // 마우스 좌클릭 뗌
         MIDDLEDOWN = 0x0020,
         MIDDLEUP = 0x0040,
         RIGHTDOWN = 0x0008,
-        RIGHTUP = 0x001
+        RIGHTUP = 0x0010
     };
     
 }
