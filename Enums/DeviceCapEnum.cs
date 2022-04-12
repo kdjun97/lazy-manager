45873063e3d85adfb99f53c70f8e60c92eb3b29e
kdjun97 <kdjun97@gmail.com>
2022-04-12 21:08:08 +0900

    Feat: Add device resolution enum

---
+++ Enums/DeviceCapEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace lazy_manager.Enums
+{
+    /// <summary>
+    /// Scale이 적용되지 않은 Device Resolution 구하기 위한 열거형
+    /// </summary>
+    public enum DeviceCap : int
+    {
+        VERTRES = 10,
+        DESKTOPVERTRES = 117
+    }
+}
