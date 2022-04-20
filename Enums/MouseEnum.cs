9e49e8e73c0dfcfab3e85e117f33049edf8a61ad
kdjun97 <kdjun97@gmail.com>
2022-04-20 20:29:57 +0900

    Comment: Add mouse enum comment

--- Enums/MouseEnum.cs
+++ Enums/MouseEnum.cs
@@ -9,10 +9,10 @@
         MOUSEMOVE = 0x0001, // 마우스 이동
         LEFTDOWN = 0x0002, // 마우스 좌클릭
         LEFTUP = 0x0004, // 마우스 좌클릭 뗌
-        MIDDLEDOWN = 0x0020,
-        MIDDLEUP = 0x0040,
-        RIGHTDOWN = 0x0008,
-        RIGHTUP = 0x0010
+        MIDDLEDOWN = 0x0020, // 마우스 휠 누름
+        MIDDLEUP = 0x0040, // 마우스 휠 뗌
+        RIGHTDOWN = 0x0008, // 마우스 우클릭
+        RIGHTUP = 0x0010 // 마우스 우클릭 뗌
     };
     
 }
