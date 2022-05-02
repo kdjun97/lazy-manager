4a4c70a3dcbd8715f727fad638ac851bb28b4211
kdjun97 <kdjun97@gmail.com>
2022-05-02 21:55:38 +0900

    Feat: Use singleton pattern DisplayResolutionModel에 singleotn pattern 적용 기존 DisplayResolution constructor 변경

--- Position/MousePosition.cs
+++ Position/MousePosition.cs
@@ -1,5 +1,5 @@
 ﻿using System.Drawing;
-using lazy_manager.Display;
+using lazy_manager.Model;
 
 namespace lazy_manager.Position
 {
@@ -8,7 +8,7 @@ namespace lazy_manager.Position
     /// </summary>
     class MousePosition
     {
-        DisplayResolution displayResolution = new DisplayResolution();
+        DisplayResolutionModel displayResolutionModel = DisplayResolutionModel.Instance();
 
         /// <summary>
         /// 마우스 x,y좌표를 얻는 함수
@@ -21,8 +21,8 @@ namespace lazy_manager.Position
             int x = int.Parse(commandSubstring[0].Substring(3));
             int y = int.Parse(commandSubstring[1].Substring(0, commandSubstring[1].Length - 1));
 
-            int mouseX = (65535 * x / displayResolution.GetDisplaySize().Width);
-            int mouseY = (65535 * y / displayResolution.GetDisplaySize().Height);
+            int mouseX = (65535 * x / displayResolutionModel.displaySize.Width);
+            int mouseY = (65535 * y / displayResolutionModel.displaySize.Height);
 
             return new Size(mouseX, mouseY);
         }
