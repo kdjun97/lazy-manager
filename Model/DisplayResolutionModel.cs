46de5c8b42c755acaaf20cd22e1b9ae1a755e238
kdjun97 <kdjun97@gmail.com>
2022-05-02 22:00:28 +0900

    Feat: Add model 화면 해상도에 관련된 모델 추가

---
+++ Model/DisplayResolutionModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace lazy_manager.Model
+{
+    class DisplayResolutionModel
+    {
+        public Size displaySize { get; set; } // display (width, height)
+        public bool isDisplayResolution { get; set; } = false; // true -> we know display info
+        public double displayScale { get; set; } // display scale (배율)
+        
+        private static DisplayResolutionModel singletonDisplayResolutionModel;
+        /// <summary>
+        /// singleton pattern
+        /// </summary>
+        public static DisplayResolutionModel Instance()
+        {
+            if (singletonDisplayResolutionModel == null)
+                singletonDisplayResolutionModel = new DisplayResolutionModel();
+            
+            return singletonDisplayResolutionModel;
+        }
+    }
+}
