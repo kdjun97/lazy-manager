39c9975d7b3df7eba49f0dd28048bd7f1aedefb6
kdjun97 <kdjun97@gmail.com>
2022-05-06 11:43:59 +0900

    Feat: Add Display Resolution Message Box 화면의 해상도 배율을 알려주는 MessageBox 추가

---
+++ FormMenu/SettingMenu/CheckDisplayResolutionHandle.cs
@@ -0,0 +1,23 @@
+﻿using lazy_manager.Display;
+using lazy_manager.Model;
+using System;
+using System.Windows.Forms;
+
+namespace lazy_manager.FormMenu.SettingMenu
+{
+    class CheckDisplayResolutionHandle : IDisposable
+    {
+        public void Dispose() {}
+
+        // 화면 해상도 배율 메시지 박스
+        public void CheckDisplayResolutionShow()
+        {
+            DisplayResolutionModel displayResolutionModel = DisplayResolutionModel.Instance(); // singleton pattern
+
+            if (!displayResolutionModel.isDisplayResolution)
+                DisplayResolution.SetDisplayResolution(); // setting display resolution
+            MessageBox.Show("해상도 배율 :" + displayResolutionModel.displayScale.ToString());
+        }
+
+    }
+}
